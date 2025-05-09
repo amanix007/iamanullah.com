28630d6e51e705352d9bdbeb802944eff44bc538
Aman Ullah <amanix007@gmail.com>
2025-05-09 13:26:38 +0600

    added google tag and analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,40 @@
+/* eslint-disable sort-imports */
 import { AnimatePresence } from 'framer-motion';
 import { ApolloProvider } from '@apollo/client';
+import { useEffect } from 'react';
 import { AppProps } from 'next/app';
 import { client } from 'apollo-client';
 import { ThemeProvider } from 'next-themes';
 
 import '../styles/index.css';
 import { Layout } from 'Templates/Layout';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
+const GA_MEASUREMENT_ID = 'G-W9304M5RCW';
+
+// Extend the global window object to include gtag
+declare global {
+	interface Window {
+		gtag: (...args: any[]) => void;
+	}
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
+	const router = useRouter();
+	useEffect(() => {
+		const handleRouteChange = (url: string) => {
+			if (typeof window.gtag === 'function') {
+				window.gtag('config', GA_MEASUREMENT_ID, {
+					page_path: url,
+				});
+			}
+		};
+
+		router.events.on('routeChangeComplete', handleRouteChange);
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange);
+		};
+	}, [router.events]);
 	return (
 		<ApolloProvider client={client}>
 			<ThemeProvider attribute="class">
@@ -17,6 +44,18 @@ function MyApp({ Component, pageProps }: AppProps) {
 						initial={false}
 						onExitComplete={() => window.scrollTo(0, 0)}
 					>
+						<Script
+							src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+							strategy="afterInteractive"
+						/>
+						<Script id="gtag-init" strategy="afterInteractive">
+							{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+						</Script>
 						<Component {...pageProps} />
 					</AnimatePresence>
 				</Layout>
